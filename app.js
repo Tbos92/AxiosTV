9ae3e1cfe77087cb5741d7141466dd0d6024240f
Tbos92 <tbos92@gmail.com>
2025-03-18 10:56:45 -0400

    Initial functionality to search and add poster for each searched show added

---
+++ app.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("searchForm");
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const searchTerm = form.elements.query.value;
+
+  const res = await axios.get(
+    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
+  );
+  const img = document.createElement("IMG");
+  img.src = res.data[0].show.image.medium;
+  document.body.append(img);
+});
