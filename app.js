974a69c6d0ac7b6f5001e4a5296396f93d4ec70c
Tbos92 <tbos92@gmail.com>
2025-03-18 11:21:41 -0400

    Refactored code to separate out the creation/population of images on the body to a separate function. Also refactored Axios to store query terms in a params object in a seaparate variable to use with the API web address

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const form = document.getElementById("searchForm");
 form.addEventListener("submit", async function (e) {
   e.preventDefault();
   const searchTerm = form.elements.query.value;
-
-  const res = await axios.get(
-    `https://api.tvmaze.com/search/shows?q=${searchTerm}`
-  );
-  const img = document.createElement("IMG");
-  img.src = res.data[0].show.image.medium;
-  document.body.append(img);
+  const config = { params: { q: searchTerm } };
+  const res = await axios.get(`https://api.tvmaze.com/search/shows`, config);
+  makeImages(res.data);
+  form.elements.query.value = "";
 });
+
+const makeImages = (shows) => {
+  for (let result of shows) {
+    if (result.show.image) {
+      const img = document.createElement("IMG");
+      img.src = result.show.image.medium;
+      document.body.append(img);
+    }
+  }
+};
